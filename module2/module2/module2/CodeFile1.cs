992173c7b66b853aaf974fe7de603814ef10794f
VogLat <23hladun.v@nltu.lviv.ua>
2024-11-21 20:24:11 +0200

    module 2

---
+++ module2/module2/module2/CodeFile1.cs
@@ -0,0 +1,106 @@
+﻿namespace FactoryMethodExample
+{
+    public abstract class Shape
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Shape(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public abstract double CalculateArea();
+        public abstract void DisplayColor();
+        public abstract (double, double) GetCenterOfCircumscribedCircle();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double x, double y, double radius) : base(x, y)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override void DisplayColor()
+        {
+            Console.WriteLine("Color: Blue");
+        }
+
+        public override (double, double) GetCenterOfCircumscribedCircle()
+        {
+            return (X, Y);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double x, double y, double width, double height) : base(x, y)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override void DisplayColor()
+        {
+            Console.WriteLine("Color: Red");
+        }
+
+        public override (double, double) GetCenterOfCircumscribedCircle()
+        {
+            return (X + Width / 2, Y + Height / 2);
+        }
+    }
+
+    public abstract class ShapeFactory
+    {
+        public abstract Shape CreateShape();
+    }
+
+    public class CircleFactory : ShapeFactory
+    {
+        private double _radius;
+
+        public CircleFactory(double radius)
+        {
+            _radius = radius;
+        }
+
+        public override Shape CreateShape()
+        {
+            return new Circle(0, 0, _radius);
+        }
+    }
+
+    public class RectangleFactory : ShapeFactory
+    {
+        private double _width, _height;
+
+        public RectangleFactory(double width, double height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public override Shape CreateShape()
+        {
+            return new Rectangle(0, 0, _width, _height);
+        }
+    }
+}
---
+++ module2/module2/module2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FactoryMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select shape to draw (1 for Circle, 2 for Rectangle, 0 to Exit): ");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 0)
+                    break;
+
+                ShapeFactory shapeFactory = null;
+                Shape shape = null;
+
+      
+                if (choice == 1)
+                {
+                    Console.WriteLine("Enter the radius of the circle: ");
+                    double radius = double.Parse(Console.ReadLine());
+                    shapeFactory = new CircleFactory(radius);
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Enter the width of the rectangle: ");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the height of the rectangle: ");
+                    double height = double.Parse(Console.ReadLine());
+                    shapeFactory = new RectangleFactory(width, height);
+                }
+
+                shape = shapeFactory.CreateShape();
+
+                Console.WriteLine($"Shape created at ({shape.X}, {shape.Y})");
+                Console.WriteLine($"Area: {shape.CalculateArea()}");
+                shape.DisplayColor();
+                var center = shape.GetCenterOfCircumscribedCircle();
+                Console.WriteLine($"Center of circumscribed circle: ({center.Item1}, {center.Item2})\n");
+            }
+        }
+    }
+}
