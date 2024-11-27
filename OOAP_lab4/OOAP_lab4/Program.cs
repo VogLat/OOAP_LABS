df81132a4c5b05b8095612ae20579da5454ec2ff
VogLat <23hladun.v@nltu.lviv.ua>
2024-11-27 17:25:09 +0200

    add adapter pattern

--- OOAP_lab4/OOAP_lab4/Program.cs
+++ OOAP_lab4/OOAP_lab4/Program.cs
@@ -9,16 +9,23 @@ class Program
     static void Main(string[] args)
     {
         IPriceCalculator priceCalculator = new PriceCalculator();
+        IPriceCalculator priceCalculator1 = new PriceCalculator();
         OrderCalculator orderCalculator = new SimpleOrderCalculator(priceCalculator);
+        OrderCalculator orderCalculator1 = new SimpleOrderCalculator(priceCalculator1);
 
         // Додавання товарів до кошика
         orderCalculator.AddItem(new ItemInBOX("Item1", 2, 100)); // 2 одиниці по 100
         orderCalculator.AddItem(new ItemInBOX("Item2", 3, 250));
+
+        orderCalculator1.AddItem(new ItemInBOX("Item1", 2, 100)); // 2 одиниці по 100
+        orderCalculator1.AddItem(new ItemInBOX("Item2", 4, 250));
         //orderCalculator.AddItem(new ItemInBOX("Item2", 1, 250));// 1 одиниця по 250
 
         // Отримання загальної вартості замовлення
         decimal totalPrice = orderCalculator.GetTotalOrderPrice();
+        decimal totalPrice1 = orderCalculator1.GetTotalOrderPrice();
         Console.WriteLine("Total Order Price: " + totalPrice); // Виведе загальну ціну
+        Console.WriteLine("Total Order Price1: " + totalPrice1); // Виведе загальну ціну
 
         Console.WriteLine("-----------------------------------");
 
